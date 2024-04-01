b8a6344c20c5e893af188d6556bb04bd3874d643
Merces-dev <giovanitjs013@gmail.com>
2024-04-01 00:31:53 -0300

    feat: create textarea story

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { TextArea } from '@stick-ui/lib';
+
+TextArea.displayName = 'TextArea';
+
+const meta: Meta<typeof TextArea> = {
+  title: 'STICK UI/Components/Core/Inputs/TextArea',
+  component: TextArea,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+export const Filled: StoryObj = {
+  args: {
+    label: 'label',
+    placeholder: 'Text here',
+    grow: true,
+    disabled: false,
+    required: true,
+  },
+};

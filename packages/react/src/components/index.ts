8fe567bf4b419e08676c998f3398a4f3b0a63403
Merces-dev <giovanitjs013@gmail.com>
2024-04-17 14:22:46 -0300

    feat: create SideBar component

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -4,4 +4,5 @@ export { PasswordInput } from './Inputs/PasswordInput';
 export { TextArea } from './Inputs/TextArea';
 export { Heading } from './Typography/Heading';
 export { Text } from './Typography/Text';
+export { SideBar, NavItem, NavSubItem, SideBarProps } from './SideBar';
 export { Icon, icons } from './Icons';

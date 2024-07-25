3ec3e24084cf955bd8c8d353325e9bd5911e7b05
Gustavo Moreno <gustavo.moreno@sp.senai.br>
2024-07-25 13:27:13 -0300

    feat: add is in intes button in dropdown menu

--- packages/react/src/components/DropdownMenu/index.tsx
+++ packages/react/src/components/DropdownMenu/index.tsx
@@ -13,6 +13,7 @@ interface DropdownMenuItem {
   iconName: keyof typeof icons;
   onClick: () => void;
   disabled?: boolean;
+  id: string; // Adicionando a propriedade id
 }
 
 interface DropdownMenuProps {
@@ -71,6 +72,7 @@ const DropdownMenu = ({
               className={buttonClasses}
               disabled={item.disabled}
               onClick={item.onClick}
+              id={item.id} // Adicionando a propriedade id ao botão
             >
               <Icon name={item.iconName} color="inherit" />
               <div className="whitespace-nowrap overflow-hidden">
